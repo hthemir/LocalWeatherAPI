a63dcfaf28dadd2dbe512e358302ce1bf2700b71
Hugo Themir <hthemir@hotmail.com>
2017-07-17 18:05:46 -0300

    beggining

---
+++ LocalWeatherAPI/LocalWeatherAPI/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace LocalWeatherAPI
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            var locator = new Geolocator();
+            locator.DesiredAccuracyInMeters = 50;
+            var position = await locator.GetGeopositionAsync();
+            await map.TrySetViewAsync(position.Coordinate.Point, 18);
+        }
+    }
+}
